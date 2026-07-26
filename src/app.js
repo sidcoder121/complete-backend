a7389588a89289582e1613718b7fe92f18c26566
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-26 17:23:22 +0530

    create a API

--- src/app.js
+++ src/app.js
@@ -2,4 +2,12 @@
 const express = require("express")
 const app = express()
 
+//create notes
+const notes = []
+
+//create API 
+app.post('/notes',(req,res)=>{
+    console.log(res.body)
+})
+
 module.exports = app
