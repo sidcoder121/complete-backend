20f97bf5e3066275f8892f77765a0eb55e74a419
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-26 17:06:30 +0530

    start the industrial leveel format

---
+++ server.js
@@ -0,0 +1,8 @@
+//Start the server
+const app = require("./src/app")
+
+
+
+app.listen(3000,()=>{
+    console.log("Server is starting at port 3000")
+})
---
+++ src/app.js
@@ -0,0 +1,5 @@
+//create a server
+const express = require("express")
+const app = express()
+
+module.exports = app
