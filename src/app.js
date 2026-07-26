ff450231b43008499bce4c4c0875db8accece32a
Siddharth Saxena <sidcoder73@gmail.com>
2026-07-26 17:45:11 +0530

    create a middleware

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 //create a server
 const express = require("express")
 const app = express()
+app.use(express.json())//middleware
 
 //create notes
 const notes = []
 
 //create API 
 app.post('/notes',(req,res)=>{
-    console.log(res.body)
+    console.log(req.body)
 })
 
 module.exports = app
